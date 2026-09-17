719442b178217c3917a9f4f64a17462f4e6aa281
Edgar Jugado <edgarjugado0505@gmail.com>
2026-09-18 07:46:02 +0800

    add refer

--- src/data/developerInfo.ts
+++ src/data/developerInfo.ts
@@ -3,10 +3,10 @@ Personal Information:
 - Name: Edgar Jugado
 - Role: Software Developer
 - Location: Binangonan, Rizal, Philippines
-- Email: ejfron.dev@gmail.com
+- Email: ejfron.dev@gmail.com or edgarjugado0516@gmail.com
 
 How they can contact me:
-- Email: ejfron.dev@gmail.com
+- Email: ejfron.dev@gmail.com or edgarjugado0516@gmail.com
 - Or click the Mail icon aside on social links to send me an email directly.
 
 How to Mute the sound effects:
@@ -16,9 +16,11 @@ Bio:
 I build clean, efficient, and user-focused web applications that solve real-world problems with code and continuous learning.
 
 Skills & Tech Stack:
-- Frontend: Vue.js, Nuxt.js, TypeScript, JavaScript, Tailwind CSS
-- Backend & Database: Node.js, Supabase, PostgreSQL, MySQL
-- Tools: Git / GitHub, REST APIs, Responsive Design
+- Frontend: Vue.js, Nuxt.js, TypeScript, JavaScript, Tailwind CSS, HTML, CSS
+- Backend & Database: PHP, Laravel, Node.js, Supabase, PostgreSQL, MySQL, Rest APIs
+-Shipping Development: Capacitor, Vite, Git, GitHub, Offline-first development, Responsive Design, Cross-browser Compatibility, UI/UX Design Principles
+-Ai & Tools: ChatGPT, Google Gemini, Claude, Figma
+
 
 Education:
 - Bachelor of Science in Information Technology (Major in Web and Mobile Application Development) at Binangonan Catholic College (2023-Present)
